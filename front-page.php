b655817711751b619da53b628e3e179690e6244e
Bearded Avenger <email@nickhaskins.com>
2014-04-16 20:41:40 -0500

    check

--- front-page.php
+++ front-page.php
@@ -24,11 +24,11 @@ get_header();
 						<?php echo the_post_thumbnail('spanky-index-cover', array('class' => 'spanky-indexpost-img spanky-img'));?>
 
 						<div class="spanky-indexpost-item-inner">
-							<p class="spanky-indexpost-meta"><?php _e('Written by','spanky');?> <?php echo get_the_author();?></p>
+							<p class="spanky-indexpost-meta"><?php apply_filters('spanky_meta_text', _e('Written by','spanky')); ?> <?php echo get_the_author();?></p>
 							<h2 class="spanky-indexpost-entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
 							
 							<div class="spanky-indexpost-item-excerpt"><?php echo wp_trim_words(get_the_excerpt(),22,'...');?></div>
-							<a class="spanky-indexpost-readmore" href="<?php the_permalink();?>"><span>Read More</span>&nbsp;<i class="spankycon spankycon-angle-right"></i></a>
+							<a class="spanky-indexpost-readmore" href="<?php the_permalink();?>"><span><?php apply_filters('spanky_readmore_text', _e('Read More','spanky')); ?></span>&nbsp;<i class="spankycon spankycon-angle-right"></i></a>
 						</div>
 
 					</article>
