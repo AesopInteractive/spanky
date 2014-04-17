3a327a83c3c4bfbe3d51be45d054b5bff88e5566
Bearded Avenger <email@nickhaskins.com>
2014-04-16 19:06:18 -0500

    check

--- front-page.php
+++ front-page.php
@@ -17,18 +17,17 @@ get_header();
 
 				while ( have_posts() ) : the_post();
 
-					$coverimg 		= wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID() ), 'spanky-tiny-cover' );
+					$coverimg 		= wp_get_attachment_image_src(get_post_thumbnail_id(get_the_ID() ), 'spanky-index-cover' );
 					?>
 
 					<article class="spanky-indexpost-item">
-						<a class="spanky-fader spanky-indexpost-item-link" href="<?php the_permalink();?>">
-							<div class="spanky-indexpost-item-inner">
-								<h2 class="spanky-indexpost-entry-title"><?php the_title();?></h2>
-								<p class="spanky-indexpost-meta">Written by <?php echo get_the_author();?></p>
-								<div class="spanky-indexpost-item-excerpt"><?php echo wp_trim_words(get_the_excerpt(),22,'...');?></div>
-							</div>
-							<div class="spanky-indexpost-item-img" style="background-image:url(<?php echo $coverimg[0];?>);background-repeat:no-repeat;background-size:cover;"></div>
-						</a>
+						<div class="spanky-indexpost-item-inner">
+							<?php the_title('<h2 class="spanky-indexpost-entry-title">','</h2>');?>
+							<p class="spanky-indexpost-meta"><?php _e('Written by','spanky');?> <?php echo get_the_author();?></p>
+							<div class="spanky-indexpost-item-excerpt"><?php echo wp_trim_words(get_the_excerpt(),22,'...');?></div>
+							<a class="spanky-indexpost-readmore" href="<?php the_permalink();?>"><span>Read More</span>&nbsp;<i class="spankycon spankycon-angle-right"></i></a>
+						</div>
+						<div class="spanky-indexpost-item-img" style="background-image:url(<?php echo $coverimg[0];?>);background-repeat:no-repeat;background-size:cover;"></div>
 					</article>
 
 					<?php
