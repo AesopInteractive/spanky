b2efdb8b2eb01892988fbbbe70c74815f2fef03d
Bearded Avenger <email@nickhaskins.com>
2014-04-21 17:17:14 -0500

    fix tag

--- partials/currently-viewing.php
+++ partials/currently-viewing.php
@@ -2,9 +2,14 @@
 
 global $wp_query;
 
-$category = $wp_query->query_vars['category_name'];
+if(is_category()) {
+	$category = $wp_query->query_vars['category_name'];
+} elseif (is_tag()){
+	$category = $wp_query->query['tag'];
+} else {
+	$category = '';
+}
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
-
 
 $results = sprintf('<span class="spanky-search-results">%s</span>',$wp_query->found_posts);
 
@@ -21,7 +26,7 @@ $results = sprintf('<span class="spanky-search-results">%s</span>',$wp_query->fo
 
 	<?php } else { ?>
 
-		<?php _e('Archives for','spanky');?> <span><?php echo $category;?></span>
+		<?php _e('Archives for ','spanky');?> <span><?php echo $category;?></span>
 
 	<?php } ?>
 
