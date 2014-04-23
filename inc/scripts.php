dc4f779d581875bb31d001867e567882b13556dc
Bearded Avenger <email@nickhaskins.com>
2014-04-23 18:30:47 -0500

    theme check stuffs

--- inc/scripts.php
+++ inc/scripts.php
@@ -39,6 +39,10 @@ class spankyScriptLoader {
 	 			)
 	 		);
 	 	}
-	 }
+
+		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		    wp_enqueue_script( 'comment-reply' );
+		}
+	}
 }
 new spankyScriptLoader;
