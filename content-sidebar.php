60b58d06dd9d39b903670431d3c298614d3d52dd
Bearded Avenger <email@nickhaskins.com>
2014-04-28 10:51:09 -0500

    check

--- content-sidebar.php
+++ content-sidebar.php
@@ -2,48 +2,51 @@
 
 <!-- Aside -->
 <aside class="andersen-content-left">
+	<div class="andersen-content-left-inner">
 
-	<?php do_action('ase_theme_sb_inside_top'); //action
+		<?php do_action('ase_theme_sb_inside_top'); //action
 
-	get_template_part('partials/brand-block');
+		get_template_part('partials/brand-block');
 
-	if( is_home() || is_archive() ){
-		do_action('ase_addon_social_links'); //action
-	}
-
-	if( is_single() ){
-		get_template_part('partials/meta-block');
-	}
-
-	if ( is_singular() ){
-
-		if(has_shortcode($post->post_content,'aesop_chapter')){ ?>
-			<div class="aesop-entry-header">
-				<h6 class="andersen-sb-heading">Chapter</h6>
-			</div>
-		<?php }
-
-		if (has_shortcode($post->post_content,'aesop_timeline_stop')){ ?>
-			<h6 class="andersen-sb-heading">Timeline</h6>
-			<?php do_action('aesop_inside_body_top');
+		if( is_home() || is_archive() ){
+			do_action('ase_addon_social_links'); //action
 		}
 
-		if( is_active_sidebar('andersen_single_sb') ) { ?>
-			<div class="andersen-sb">
-				<?php dynamic_sidebar('andersen_single_sb'); ?>
-			</div>
-		<?php }
+		if( is_single() ){
+			get_template_part('partials/meta-block');
+		}
+
+		if ( is_singular() ){
+
+			if(has_shortcode($post->post_content,'aesop_chapter')){ ?>
+				<div class="aesop-entry-header">
+					<h6 class="andersen-sb-heading">Chapter</h6>
+				</div>
+			<?php }
+
+			if (has_shortcode($post->post_content,'aesop_timeline_stop')){ ?>
+				<h6 class="andersen-sb-heading">Timeline</h6>
+				<?php do_action('aesop_inside_body_top');
+			}
+
+			if( is_active_sidebar('andersen_single_sb') ) { ?>
+				<div class="andersen-sb">
+					<?php dynamic_sidebar('andersen_single_sb'); ?>
+				</div>
+			<?php }
 
 
-	}
+		}
 
-	if( is_home() || is_archive() && is_active_sidebar('andersen_sb') ) { ?>
-	<div class="andersen-sb">
-		<?php dynamic_sidebar('andersen_sb'); ?>
+		if( is_home() || is_archive() && is_active_sidebar('andersen_sb') ) { ?>
+		<div class="andersen-sb">
+			<?php dynamic_sidebar('andersen_sb'); ?>
+		</div>
+		<?php } ?>
+
+		<?php do_action('ase_theme_sb_inside_bottom'); //action ?>
+
 	</div>
-	<?php } ?>
-
-	<?php do_action('ase_theme_sb_inside_bottom'); //action ?>
 
 </aside>
 <a class="andersen-sb-toggle" href="#">Open</a>
